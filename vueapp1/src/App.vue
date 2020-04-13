22ba4c21bf13e825765c95ecd8a7272e300787bf
blacklisten <756319278@qq.com>
2020-04-13 23:43:55 +0800

    single-spa for vue demo

---
+++ vueapp1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <span id="vueapp1">
+    <p>App1 is working!</p>
+    <p>comment me on and off to see HMR</p>
+    <router-link to="/vueapp2/about">go to vueapp2 about</router-link>
+    <router-view></router-view>
+  </span>
+</template>
+
+<style>
+#vueapp1 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vueapp1/src/router.js
@@ -0,0 +1,14 @@
+import Router from 'vue-router'
+import Home from './views/Home'
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/vueapp1',
+      name: 'home',
+      component: Home
+    },
+  ]
+})
